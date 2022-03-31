844a5a8db1e0d07721ebc80a8664c652e7e0c070
dawit.abera <dawit.y.abera@nordstrom.com>
2022-03-30 23:26:07 -0700

    initial setup

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf(fmt.Sprintf("Server running on port:: %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
